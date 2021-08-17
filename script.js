8215ed6c9baa422c21b9fd1ba44113988a1a6ca4
abubakaromar18 <37700067+abubakaromar18@users.noreply.github.com>
2021-08-17 14:02:26 -0500

    lastest Version

---
+++ script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if (this.scrollY > 10) {
+            $('.navbar').addClass('sticky');
+            //$('.home').addClass('sticky');
+        } else {
+            $('.navbar').removeClass('sticky');
+            //$('.home').removeClass('sticky');
+        }
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass('show');
+        }
+        else{
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+
+    // SCROLL UP SCRIPT
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+
+    // MENU BUTTON TOGGLE
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    //TYPING ANIMATION SCRIPT
+    var typed = new Typed(".typing",{
+        strings: ["PROGRAMMER", "DESIGNER", "FREELANCER"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: ["PROGRAMMER", "DESIGNER", "FREELANCER"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+
+
+    // OWL CAROUSEL SCRIPT
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
